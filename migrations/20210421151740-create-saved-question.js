7a4c473b4fbba6a21885470c08d523fe8a2e31a6
Yairderry <77237113+Yairderry@users.noreply.github.com>
2021-05-26 22:31:04 +0300

    More renaming tables and models

--- migrations/20210421151740-create-saved-question.js
+++ migrations/20210421151740-create-saved-question.js
@@ -1,54 +1,54 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Saved_questions', {
+    await queryInterface.createTable("Saved_Questions", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       question: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       option_1: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       option_2: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       option_3: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       option_4: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       answer: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       rating: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
       },
       vote_count: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       answered_correct: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       answered_wrong: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Saved_questions');
-  }
+    await queryInterface.dropTable("Saved_Questions");
+  },
 };
